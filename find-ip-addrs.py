55c28c5b94376863af22689a58a85af879bd035b
Kyle Laker <kyle@laker.email>
2023-02-15 00:51:33 -0500

    fix(find-ip-addrs): remove use of deprecated service clients

--- find-ip-addrs.py
+++ find-ip-addrs.py
@@ -5,9 +5,19 @@ Find available IP addresses in an AWS Subnet
 """
 
 import ipaddress
+from typing import Generator
 
 import boto3
 import click
+
+from mypy_boto3_ec2 import EC2Client
+from mypy_boto3_ec2.type_defs import NetworkInterfaceTypeDef
+
+
+def get_network_interfaces(ec2: EC2Client, subnet_id: str) -> Generator[NetworkInterfaceTypeDef, None, None]:
+    paginator = ec2.get_paginator('describe_network_interfaces')
+    for page in paginator.paginate(Filters=[{'Name': 'subnet-id', 'Values': [subnet_id]}]):
+        yield from page['NetworkInterfaces']
 
 
 @click.command('find-ip-addrs')
@@ -28,6 +38,7 @@ import click
 @click.option(
     '--profile',
     '-p',
+    envvar='AWS_PROFILE',
     required=True,
     default='default',
     help='Profile name',
@@ -44,15 +55,13 @@ def main(ctx: click.Context, subnet_id: str, subnet_name: str, profile: str) -> 
         return
 
     session = boto3.Session(profile_name=profile)
-    ec2 = session.resource('ec2')
+    ec2 = session.client('ec2')
+    query = {}
     if subnet_id:
-        subnets = [ec2.Subnet(subnet_id)]
+        query["SubnetIds"] = [subnet_id]
     elif subnet_name:
-        subnets = ec2.subnets.filter(
-            Filters=[{'Name': 'tag:Name', 'Values': [subnet_name]}]
-        )
-    else:
-        subnets = ec2.subnets.all()
+        query["Filters"] = [{'Name': 'tag:Name', 'Values': [subnet_name]}]
+    subnets = ec2.describe_subnets(**query)['Subnets']
 
     for subnet in subnets:
         # AWS reserves the network address, three more addresses, and the broadcast address.
@@ -60,19 +69,26 @@ def main(ctx: click.Context, subnet_id: str, subnet_name: str, profile: str) -> 
         #  https://docs.aws.amazon.com/vpc/latest/userguide/VPC_Subnets.html#VPC_Sizing
         # Python's ipaddress.IPNetwork does not support slicing, so it needs to be converted to
         # a list
-        available_ips = {str(ip) for ip in list(ipaddress.IPv4Network(subnet.cidr_block))[4:-1]}
+        id = subnet.get('SubnetId')
+        cidr = subnet.get('CidrBlock')
+        if not id:
+            raise Exception('Unexpectedly failed to retrieve subnet ID from EC2 API')
+        if not cidr:
+            raise Exception('Only IPv4 is currently supported')
+        available_ips = {str(ip) for ip in list(ipaddress.IPv4Network(cidr))[4:-1]}
 
-        name_tags = [tag for tag in subnet.tags if tag['Key'] == 'Name']
+        name_tags = [tag for tag in subnet.get('Tags', []) if tag.get('Key') == 'Name']
         if name_tags:
-            name = name_tags[0]['Value']
+            name = name_tags[0].get('Value')
         else:
             name = None
 
-        for interface in subnet.network_interfaces.all():
-            int_ips = {addr['PrivateIpAddress'] for addr in interface.private_ip_addresses}
+        interfaces = get_network_interfaces(ec2, id)
+        for interface in interfaces:
+            int_ips = {addr.get('PrivateIpAddress') for addr in interface.get('PrivateIpAddresses', [])}
             available_ips -= int_ips
 
-        identifier = f"{name} ({subnet.id})" if name else subnet.id
+        identifier = f"{name} ({id})" if name else id
 
         click.echo(f"{len(available_ips)} available IP Addresses in {identifier}:")
         for ip in sorted(available_ips, key=ipaddress.IPv4Address):
